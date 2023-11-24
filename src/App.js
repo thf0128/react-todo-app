9561639b3a8e4b8f2ed492859ec0ed21fc78cb08
KimHanSol <thf0128@naver.com>
2023-11-24 17:19:39 +0900

    # 8. 프로젝트 구조 변경 및 React Router 사용 방법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import TodoTemplate from './components/TodoTemplate';
+import TodoTemplate from './components/todo/TodoTemplate';
 import './App.css';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Join from './components/user/Join';
+import Login from './components/user/Login';
 
 const App = () => {
-  return <TodoTemplate />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={<TodoTemplate />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/join'
+          element={<Join />}
+        />
+      </Routes>
+      <Footer />;
+    </>
+  );
 };
 
 export default App;
